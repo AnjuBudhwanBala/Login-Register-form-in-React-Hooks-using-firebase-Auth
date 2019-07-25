8003a68a72830c9c10f159d9f606b7eae6b173c7
Anju Bala <32696738+AnjuBudhwanBala@users.noreply.github.com>
2019-07-25 13:08:20 +0200

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/database";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyC7PuV3WxHkcpqJHgG7DJU1InSe6-DrJVg",
-  authDomain: "react-chat-app-20517.firebaseapp.com",
-  databaseURL: "https://react-chat-app-20517.firebaseio.com",
-  projectId: "react-chat-app-20517",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "",
   messagingSenderId: "437258096120",
-  appId: "1:437258096120:web:543c0541fabaddc9"
+  appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
